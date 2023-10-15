50b9ce71cd2bc0cb7ebd2d0897ae6c26b51465c6
erinlacaze <111822014+erinlacaze@users.noreply.github.com>
2023-10-14 20:59:13 -0700

    Update client.py

--- HA03/client.py
+++ HA03/client.py
@@ -11,9 +11,9 @@ def message(cSocket, msg):
 def file(cSocket, file):
     with open(file, 'rb') as file1:
         file2 = file1.read(H_SIZE)
-        while file2:
+        while file2: # there is still stuff in the file to read
             cSocket.send(file2)
-            file2 = file1.read(H_SIZE)
+            file2 = file1.read(H_SIZE) # read 1024 bytes at a time
 
 userInput = ""
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,12 +25,12 @@ messageThread.start()
 
 while True:
     userInput = input()
-    if userInput.startswith("/"):
+    if userInput.startswith("exit"): # close client
+        break
+    elif userInput.startswith("/"): # read file and send output in packets of 1024 bytes
         find = userInput[1:]
         file(client, find)
-    elif userInput.startswith("exit"):
-        break
-    else:
-        message(client, userInput)
+    else: # send message
+        message(client, userInput) 
 
 client.close()
